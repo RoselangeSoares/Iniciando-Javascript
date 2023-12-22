0641ffec9ecac6e81ec7f4ff2781940149861285
RoselangeSoares <roselange_boll@hotmail.com>
2023-12-22 00:58:34 -0300

    .

---
+++ play.js
@@ -0,0 +1,10 @@
+//mensagem de saída
+console.log('Digite o nome do seu jogador')
+
+//Declarando uma variável
+let nickname = 'Soares Mestre das Almas'
+
+//Concantenando uma mensagem fixa com uma variável
+console.log('Bem Vindo ' + nickname)
+console.log(nickname + ' Entrou no servidor!')
+
